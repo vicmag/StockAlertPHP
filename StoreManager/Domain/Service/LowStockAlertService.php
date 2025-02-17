c1f4b2b79019c6f73ce1a331e2fa2cc8c485a7e9
vicmag <vicmag@hotmail.com>
2025-02-17 21:38:53 +0000

    Refactoring Phase v1.0

--- StoreManager/Domain/Service/LowStockAlertService.php
+++ StoreManager/Domain/Service/LowStockAlertService.php
@@ -2,6 +2,7 @@
 
 namespace StoreManager\Domain\Service;
 
+use StoreManager\Domain\Model\Product;
 use StoreManager\Domain\Repository\ProductRepositoryInterface;
 
 class LowStockAlertService
@@ -16,6 +17,11 @@ class LowStockAlertService
     public function checkLowStock(string $productId): bool
     {
         $product = $this->productRepository->findById($productId);
+        return $this->isLowStock($product);
+    }
+    
+    private function isLowStock(Product $product): bool
+    {
         return $product->getCurrentStock() <= $product->getLowStockThreshold();
     }
 }
